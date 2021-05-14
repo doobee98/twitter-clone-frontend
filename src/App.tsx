f8b3c4016a96018599e5656b23666d227c263e2a
2doo <doobee98@naver.com>
2021-05-15 00:48:41 +0900

    setting: styled-components and GlobalStyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
-function App() {
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+
+  html {
+    font-size: 16px;
+    height: 100%;
+  }
+
+  body {
+    width: 100%;
+    min-height: 100%;
+    margin: 0;
+    padding: 0;
+  }
+
+  input {
+    appearance: none;
+    border-radius: 0;
+  }
+
+  p {
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+    padding-inline-start: 0;
+    padding: 0; 
+    margin: 0;
+  }
+
+  li { 
+    padding: 0;
+    margin:0px; 
+  }
+
+  a:link { color: inherit; text-decoration: none;}
+  a:visited { color: inherit; text-decoration: none;}
+  a:hover { color: inherit; text-decoration: none;}
+
+  input,
+  textarea,
+  button,
+  select,
+  a {
+    text-decoration: none;
+    outline: none;
+  }
+
+  #root {
+    height: 100%;
+  }
+`;
+
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      Hello, React!
+    </>
   );
-}
+};
 
 export default App;
