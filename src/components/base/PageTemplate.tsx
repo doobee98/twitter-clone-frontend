047465da85f1f6894481e69aab02233e3e236f7d
2doo <doobee98@naver.com>
2021-05-15 20:09:56 +0900

    style: PageTemplate and PageTemplate ContentWrapper

--- src/components/base/PageTemplate.tsx
+++ src/components/base/PageTemplate.tsx
@@ -1,22 +1,46 @@
+import styled, { css } from 'styled-components';
 import useTitle from 'hooks/useTitle';
+import { ColorPalette } from 'utils/colorUtils';
 import ExploreSideBar from './ExploreSideBar';
 import NavigationSideBar from './NavigationSideBar';
 
+const PageTemplateContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+interface ContentWrapperProps {
+  width?: string;
+}
+
+const ContentWrapper = styled.div<ContentWrapperProps>`
+  min-height: 100vh;
+  border-left: 1px solid ${ColorPalette.GRAY_E6};
+  border-right: 1px solid ${ColorPalette.GRAY_E6};
+
+  ${(props) =>
+    css`
+      width: ${props.width ?? '598px'};
+    `}
+`;
+
 interface PageTemplateProps {
   title: string;
+  contentWidth?: string;
 }
 
 const PageTemplate: React.FC<PageTemplateProps> = (props) => {
-  const { children, title } = props;
+  const { children, title, contentWidth } = props;
 
   useTitle(`${title} / Twitter-Clone`);
 
   return (
-    <>
+    <PageTemplateContainer>
       <NavigationSideBar />
-      {children}
+      <ContentWrapper width={contentWidth}>{children}</ContentWrapper>
       <ExploreSideBar />
-    </>
+    </PageTemplateContainer>
   );
 };
 
