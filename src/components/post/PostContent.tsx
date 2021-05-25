efe4650dea46d52b01080c9c899ba1ce48febd4f
beom <calvin0627@naver.com>
2021-05-25 16:20:56 +0900

    FEAT: tweet post textarea style, and size of textarea changes dynamically by input text length

--- src/components/post/PostContent.tsx
+++ src/components/post/PostContent.tsx
@@ -1,4 +1,5 @@
-import styled from 'styled-components';
+import { useEffect, useRef, useState } from 'react';
+import styled, { css } from 'styled-components';
 import { ColorPalette } from 'utils/colorUtils';
 import ToolList from './ToolList';
 
@@ -12,6 +13,22 @@ const TextWrapper = styled.textarea`
   width: 100%;
   min-height: 56px;
   padding: 12px 0px;
+
+  overflow: hidden;
+  resize: none;
+  border: none;
+
+  font-size: 20px;
+  font-family: '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto',
+    Helvetica, Arial, sans-serif;
+  font-weight: 400;
+
+  ::placeholder {
+    font-size: 20px;
+    font-family: '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto',
+      Helvetica, Arial, sans-serif;
+    font-weight: 400;
+  }
 `;
 
 const PermissionWrapper = styled.div`
@@ -28,9 +45,32 @@ const ToolBarWrapper = styled.div`
 `;
 
 const PostContent: React.FC = () => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [currentValue, setCurrentValue] = useState('');
+  const [writing, setWriting] = useState(false);
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = '0px';
+      textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+    }
+  }, [currentValue]);
+
   return (
     <ContentContainer>
-      <TextWrapper />
+      <TextWrapper
+        ref={textareaRef}
+        value={currentValue}
+        placeholder="What's happening?"
+        rows={1}
+        defaultValue=""
+        onClick={() => {
+          setWriting(true);
+        }}
+        onChange={(e) => {
+          setCurrentValue(e.target.value);
+        }}
+      />
       <PermissionWrapper />
       <ToolBarWrapper>
         <ToolList />
