029f9c58ca1b5384955b6edcfa3da3fb6e2beada
beom <calvin0627@naver.com>
2021-05-25 16:15:28 +0900

    ADD: style of post type icons and button

--- src/components/post/ToolList.tsx
+++ src/components/post/ToolList.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import { ColorPalette, hexToRgbA } from '../../utils/colorUtils';
+import { ColorPalette } from '../../utils/colorUtils';
+import { BasicType } from '../../utils/iconUtils';
+import Button from '../base/Button';
+import Icon from '../base/Icon';
 
 const ToolListContainer = styled.div`
   height: 100%;
@@ -16,9 +19,14 @@ const ToolWrapper = styled.div`
   justify-content: center;
   width: 38px;
   height: 38px;
-
-  background-color: ${ColorPalette.LIGHTDARK};
   border-radius: 19px;
+`;
+
+const ToolIcon = styled(Icon)`
+  width: 60%;
+  height: 60%;
+  align-items: center;
+  justify-content: center;
 `;
 
 const ButtonWrapper = styled.div`
@@ -27,20 +35,72 @@ const ButtonWrapper = styled.div`
   justify-content: center;
   width: 15%;
   height: 100%;
-  background-color: ${ColorPalette.SKYBLUE_DARK};
+  display: flex;
+  align-items: flex-end;
+  shape-outside: inset(calc(100% - 100px) 0 0);
+`;
+
+const TweetButton = styled(Button)`
+  float: right;
+  width: 100%;
+  color: ${ColorPalette.WHITE};
+  background-color: ${ColorPalette.SKYBLUE};
+  font-size: 16px;
+  font-weight: bold;
+
+  &:hover {
+    background-color: ${ColorPalette.SKYBLUE_DARK};
+  }
 `;
 
 const ToolList = () => {
   return (
     <>
       <ToolListContainer>
-        <ToolWrapper />
-        <ToolWrapper />
-        <ToolWrapper />
-        <ToolWrapper />
-        <ToolWrapper />
+        <ToolWrapper>
+          <ToolIcon
+            iconType={BasicType.MEDIA}
+            size={36}
+            color={ColorPalette.SKYBLUE}
+            hasCircleWrapper
+          />
+        </ToolWrapper>
+        <ToolWrapper>
+          <Icon
+            iconType={BasicType.GIF}
+            size={36}
+            color={ColorPalette.SKYBLUE}
+            hasCircleWrapper
+          />
+        </ToolWrapper>
+        <ToolWrapper>
+          <Icon
+            iconType={BasicType.POLL}
+            size={36}
+            color={ColorPalette.SKYBLUE}
+            hasCircleWrapper
+          />
+        </ToolWrapper>
+        <ToolWrapper>
+          <Icon
+            iconType={BasicType.EMOJI}
+            size={36}
+            color={ColorPalette.SKYBLUE}
+            hasCircleWrapper
+          />
+        </ToolWrapper>
+        <ToolWrapper>
+          <Icon
+            iconType={BasicType.SCHEDULE}
+            size={36}
+            color={ColorPalette.SKYBLUE}
+            hasCircleWrapper
+          />
+        </ToolWrapper>
       </ToolListContainer>
-      <ButtonWrapper />
+      <ButtonWrapper>
+        <TweetButton> Tweet </TweetButton>
+      </ButtonWrapper>
     </>
   );
 };
