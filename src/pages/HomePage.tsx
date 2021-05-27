500f6e31037c386a4c7d6bd66ee9577ae8059a30
beom <calvin0627@naver.com>
2021-05-28 01:35:21 +0900

    REFACTOR(components): remove test wrapper

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,14 +3,10 @@ import styled from 'styled-components';
 import PageTemplate from 'components/base/PageTemplate';
 import TweetPost from 'components/post/TweetPost';
 
-const ToBeRemovedWrapper = styled(React.Fragment)``;
-
 const HomePage: React.FC = () => {
   return (
     <PageTemplate title="Home">
-      <ToBeRemovedWrapper>
-        <TweetPost />
-      </ToBeRemovedWrapper>
+      <TweetPost />
     </PageTemplate>
   );
 };
