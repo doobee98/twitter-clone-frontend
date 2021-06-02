2e02e84217a93f90b51e399cac923780b5fc3cb1
JJGwan926 <marong142@gmail.com>
2021-06-02 13:40:12 +0900

    REFACTOR(utils): rename as useInfinityScroll

---
+++ src/hooks/useInfinityScroll.ts
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+import TweetModel from '../models/tweet';
+
+const useInfinityScroll = (
+  defaultTweets: TweetModel[],
+  tweets: TweetModel[],
+  setTweets: React.Dispatch<React.SetStateAction<TweetModel[]>>,
+) => {
+  const infScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (tweets.length >= defaultTweets.length) {
+      return;
+    }
+
+    if (scrollTop + clientHeight === scrollHeight) {
+      const newTweets: TweetModel[] = [...tweets, defaultTweets[tweets.length]];
+
+      setTweets(newTweets);
+    }
+  };
+
+  useEffect(() => {
+    // when mount
+    window.addEventListener('scroll', infScroll);
+
+    return () => {
+      // when unmount
+      window.removeEventListener('scroll', infScroll);
+    };
+  });
+};
+
+export default useInfinityScroll;
