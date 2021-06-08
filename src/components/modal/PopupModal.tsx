523383f3b2f93a1a185ce3f94aa925e8e98979b9
beom <calvin0627@naver.com>
2021-06-09 03:52:43 +0900

    PROTO(components): implement popupmodal prototype

---
+++ src/components/modal/PopupModal.tsx
@@ -0,0 +1,61 @@
+import useClickOutside from 'hooks/useClickOutside';
+import { Children, useEffect, useRef, useState } from 'react';
+import styled from 'styled-components';
+import { ColorPalette, hexToRgbA } from 'utils/colorUtils';
+import { BasicType } from 'utils/iconUtils';
+import NavItem from '../base/NavItem';
+import Modal from '../base/Modal';
+
+const PopupHeader = styled.div`
+  padding: 0;
+`;
+
+const CloseButton = styled(NavItem)`
+  color: ${ColorPalette.SKYBLUE};
+  width: 40px;
+`;
+
+interface PopupModalHeaderProps {
+  onClose: () => void;
+}
+
+const PopupModalHeader: React.FC<PopupModalHeaderProps> = (props) => {
+  const { onClose } = props;
+  return (
+    <PopupHeader onClick={onClose}>
+      <CloseButton iconType={BasicType.CLOSE} />
+    </PopupHeader>
+  );
+};
+
+interface PopupModalProps {
+  width: number;
+  isOpened: boolean;
+  setIsOpened: React.Dispatch<React.SetStateAction<boolean>>;
+  className?: string;
+}
+
+const PopupModal: React.FC<PopupModalProps> = (props) => {
+  const { isOpened, width, setIsOpened, className, children } = props;
+
+  const closePopup = () => {
+    setIsOpened(false);
+  };
+
+  return (
+    <>
+      <Modal
+        position={[0, 0]}
+        width={width}
+        isLocked
+        isOpened={isOpened}
+        setIsOpened={setIsOpened}
+        Header={<PopupModalHeader onClose={() => closePopup()} />}
+      >
+        {children}
+      </Modal>
+    </>
+  );
+};
+
+export default PopupModal;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import PageTemplate from 'components/base/PageTemplate';
 import TweetList from 'components/tweet/TweetList';
 import TweetPost from 'components/post/TweetPost';
+// REMOVE
+import Button from 'components/base/Button';
+import PopupModal from 'components/modal/PopupModal';
+
+// REMOVED
+const ToBeRemovedTestButton = styled(Button)`
+  margin: 0;
+  border: 1px solid black;
+  width: 300px;
+  height: 100px;
+`;
 
 const HomePage: React.FC = () => {
+  // REMOVED
+  const [isOpened, setIsOpened] = useState(false);
+  const openPopup = () => {
+    setIsOpened(true);
+  };
+  const closePopup = () => {
+    setIsOpened(false);
+  };
+
   return (
     <PageTemplate title="Home">
       <div>Temporary Home Page</div>
+
+      {/* REMOVED */}
+      <ToBeRemovedTestButton onClick={openPopup}>
+        팝업테스트용버튼
+      </ToBeRemovedTestButton>
+      <PopupModal isOpened={isOpened} setIsOpened={setIsOpened} width={600}>
+        <TweetPost />
+      </PopupModal>
+
       <TweetPost />
       <TweetList />
     </PageTemplate>
