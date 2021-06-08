3ac6fa758e88b0384b90960617ad404bdb7a839d
beom <calvin0627@naver.com>
2021-06-09 03:42:14 +0900

    FEAT(hooks): make hook when click outside of modal popup

---
+++ src/hooks/useClickOutside.ts
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+const useClickOutside = (
+  ref: React.RefObject<HTMLElement>,
+  onClickOutside: () => void,
+  exceptRef?: React.RefObject<HTMLElement>,
+): void => {
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (
+        ref.current &&
+        !ref.current.contains(event.target as Node) &&
+        !exceptRef?.current?.contains(event.target as Node)
+      ) {
+        onClickOutside();
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [ref]);
+};
+
+export default useClickOutside;
