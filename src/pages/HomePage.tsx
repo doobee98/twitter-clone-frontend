735523570278ca5fb83fcafe71aea23ca7cc7b7f
2doo <doobee98@naver.com>
2021-06-12 00:16:06 +0900

    FEAT(components): refine PageTemplate
    * ContentTemplate, ContentHeader, ContentSection 도입
    * 오른쪽 사이드바인 ExploreSideBar를 PageTemplate 기본형에서 제거

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,13 +3,37 @@ import styled from 'styled-components';
 import PageTemplate from 'components/base/PageTemplate';
 import TweetList from 'components/tweet/TweetList';
 import TweetPost from 'components/post/TweetPost';
+import ExploreSideBar from 'components/base/ExploreSideBar';
+import ContentTemplate, {
+  ContentHeader,
+  ContentSection,
+} from 'components/base/ContentTemplate';
+import { ColorPalette } from 'utils/colorUtils';
+
+const SpaceSection = styled(ContentSection)`
+  background-color: ${ColorPalette.GRAY_F9};
+  height: 12px;
+`;
 
 const HomePage: React.FC = () => {
   return (
     <PageTemplate title="Home">
-      <div>Temporary Home Page</div>
-      <TweetPost />
-      <TweetList />
+      <ContentTemplate>
+        <ContentHeader>
+          <strong>Home</strong>
+        </ContentHeader>
+        <ContentSection>
+          <TweetPost />
+        </ContentSection>
+        <SpaceSection />
+        <TweetList />
+      </ContentTemplate>
+      <ContentTemplate width="300px" hideBorder>
+        <ContentHeader hideBorder>
+          <strong>SearchBar</strong>
+        </ContentHeader>
+        <ExploreSideBar />
+      </ContentTemplate>
     </PageTemplate>
   );
 };
