d1b6966324f54d6290eb8727b29a8fd9b1da270d
JJGwan926 <marong142@gmail.com>
2021-06-14 14:48:26 +0900

    FEATURE(components, hooks) : init fetch and infinityScroll w/ throttling

--- src/components/tweet/TweetList.tsx
+++ src/components/tweet/TweetList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 import { useAppDispatch, useHomeSelector } from 'hooks/redux';
 import { fetchFeed } from 'modules/home';
@@ -13,8 +13,6 @@ const TweetListContainer = styled.div`
 `;
 
 const TweetList: React.FC = () => {
-  const [isInit, setIsInit] = useState(true);
-
   const homeStore = useHomeSelector();
   const dispatch = useAppDispatch();
   const { feed } = homeStore;
@@ -23,18 +21,22 @@ const TweetList: React.FC = () => {
     dispatch(fetchFeed());
   };
 
-  // init fetch
-  if (isInit) {
-    setIsInit(false);
-    handleFetchFeed();
-  }
-
-  useInfinityScroll(feed, handleFetchFeed);
+  // MAYBE TOBE REFACTORED
+  const timer = useRef<boolean>(false);
+  const setTimer = (inputBoolean: boolean) => {
+    timer.current = inputBoolean;
+  };
+  useInfinityScroll(handleFetchFeed, timer, setTimer);
 
   // TO BE REMOVED
   useEffect(() => {
     console.log(feed);
   }, [feed]);
+
+  // INIT FETCH
+  useEffect(() => {
+    handleFetchFeed();
+  }, []);
 
   return (
     <TweetListContainer>
--- src/hooks/useInfinityScroll.ts
+++ src/hooks/useInfinityScroll.ts
@@ -1,16 +1,33 @@
-import { fetchFeed } from 'modules/home';
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Tweet from '../models/tweet';
-import { useAppDispatch } from './redux';
 
-const useInfinityScroll = (feed: Tweet[], scrollFunction: () => any) => {
+const useInfinityScroll = (
+  scrollFunction: () => Promise<any>,
+  timer: React.MutableRefObject<boolean>,
+  setTimer: (inputBoolean: boolean) => void,
+) => {
   const infScroll = () => {
     const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
 
-    // need debouncing
-    if (scrollTop + clientHeight === scrollHeight) {
-      scrollFunction();
+    console.log('inf');
+
+    // THROTTLING
+    // it there is timer, do not execute.:
+    if (timer.current) {
+      console.log('return');
+      return;
     }
+
+    if (scrollTop + clientHeight >= scrollHeight * 0.8) {
+      console.log('------------new------------');
+      setTimer(true);
+      setTimeout(() => {
+        scrollFunction();
+        setTimer(false);
+      }, 2000);
+    }
+
+    console.log('end');
   };
 
   useEffect(() => {
