d44ed9a2ced3f4b3beb2b9795356d64f9592e975
2doo <doobee98@naver.com>
2021-06-15 01:12:44 +0900

    FEAT(apis): UsersApi

---
+++ src/apis/UsersApi.ts
@@ -0,0 +1,28 @@
+import { AxiosPromise } from 'axios';
+import User from 'models/user';
+import Api from './Api';
+import ApiBuilder from './ApiBuilder';
+
+const apiPrefix = 'http://localhost:8000/api';
+const usersApiPrefix = `${apiPrefix}/users`;
+
+class UsersApi extends Api {
+  apiEndPoints = {
+    USER: (id: string) => `${usersApiPrefix}/${id}`,
+  };
+
+  private static _instance: UsersApi;
+
+  static get instance(): UsersApi {
+    if (this._instance === undefined) {
+      this._instance = new UsersApi();
+    }
+    return this._instance;
+  }
+
+  login(id: string): AxiosPromise<User> {
+    return ApiBuilder.create().get().url(this.apiEndPoints.USER(id)).build();
+  }
+}
+
+export default UsersApi;
