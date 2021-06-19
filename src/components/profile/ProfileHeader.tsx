8323f9b2096a2b54baa1542e115fa25c39a44ee0
beom <calvin0627@naver.com>
2021-06-20 05:32:42 +0900

    FEAT(component): implement profile page biography header prototype

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,5 +1,68 @@
-const ProfileHeader: React.FC = () => {
-  return <strong>ProfileHeader Proto</strong>;
+import User from 'models/user';
+import styled from 'styled-components';
+import { hexToRgbA, ColorPalette } from 'utils/colorUtils';
+import Profile from '../base/Profile';
+import FollowButton from './FollowButton';
+
+const ProfileHeaderContainer = styled.div`
+  width: 106%;
+  margin-left: -3%;
+  margin-right: -3%;
+`;
+
+const ProfileUpperBackground = styled.div`
+  display: absolute;
+  width: auto;
+  height: 200px;
+  background-color: ${hexToRgbA(ColorPalette.BLACK, 0.4)};
+`;
+
+// TODO : After Profile Size Refactor
+const ProfileImageWrapper = styled.div`
+  position: relative;
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  left: 10px;
+  top: 140px;
+
+  background-color: ${ColorPalette.GREEN};
+`;
+
+const ProfileTooltipContainer = styled.div`
+  position: relative;
+  top: 60%;
+  text-align: right;
+`;
+
+const PorfileTooltipItem = styled.div`
+  display: inline-block;
+  margin: 5px;
+`;
+
+interface ProfileHeaderProps {
+  user: User;
+  isMine: boolean;
+}
+
+const ProfileHeader: React.FC<ProfileHeaderProps> = (props) => {
+  const { user, isMine } = props;
+
+  return (
+    <ProfileHeaderContainer>
+      <ProfileUpperBackground>
+        <ProfileImageWrapper />
+        <ProfileTooltipContainer>
+          <PorfileTooltipItem>
+            {!isMine && <FollowButton user={user} />}
+          </PorfileTooltipItem>
+          <PorfileTooltipItem>
+            {!isMine && <FollowButton user={user} />}
+          </PorfileTooltipItem>
+        </ProfileTooltipContainer>
+      </ProfileUpperBackground>
+    </ProfileHeaderContainer>
+  );
 };
 
 export default ProfileHeader;
